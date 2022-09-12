762a86635ba15fbfe9e97ace67aeb907113d0e1a
ofrezucker <112804727+ofrezucker@users.noreply.github.com>
2022-09-12 11:01:51 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    radio.setGroup(1)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.setGroup(1)
+})
+basic.forever(function () {
+	
+})
